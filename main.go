1998e1ceb029f02d83a695906ffdb7ce4434bb69
wutulin <913221825@qq.com>
2021-01-13 18:22:22 +0800

    角色struct配置

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package gin_admin
 
 import (
+	"fmt"
 	"log"
 	"strings"
 
@@ -60,6 +61,9 @@ func ParseSchema(n models.AdminOperation) error {
 	schemaField := model.Statement.Schema.FieldsByName
 	newFields := make(map[string]*global.Field)
 	for _, v := range schemaField {
+		if v.DBName == "" {
+			continue
+		}
 		field := ParseTag(v)
 		newFields[v.DBName] = field
 	}
@@ -90,6 +94,9 @@ func ParseTag(v *schema.Field) (m *global.Field) {
 		case "name":
 			m.Name = rr[1]
 		}
+	}
+	if v.DBName == "" {
+		fmt.Println("")
 	}
 	return
 }
